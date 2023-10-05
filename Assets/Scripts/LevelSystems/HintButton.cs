123a72cdb9583ddadf5551bb2c79d9526e582cbe
Quang Nguyen <qtn1113@gmail.com>
2023-10-05 15:45:00 -0500

    v1.10
    -Reworked Hint system so that hints are immediately available, and player can activate hints at any time as long as there are hints remaining and they have not placed a plant on the board

--- Assets/Scripts/LevelSystems/HintButton.cs
+++ Assets/Scripts/LevelSystems/HintButton.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class HintButton : MonoBehaviour
 {
     HintTracker hintTracker;
-    [SerializeField] GameObject hintButton;
+    [SerializeField] Image hintButton;
+    [SerializeField] Button enableButton; //used to disable button functionality
+    [SerializeField] HoverUI hoverScript; //used to disable hovering animations when hints are disabled
     [SerializeField] LevelManager lvMan;
     //general hint info
     [SerializeField] int numHints; //total number of available hints for this level
@@ -14,6 +18,11 @@ public class HintButton : MonoBehaviour
 
     //audio for button click
     AudioSource buttonAudio;
+    // text for showing number of hints remaining
+    [SerializeField] Image numHintsDisplay;
+    [SerializeField] TextMeshProUGUI displayNumHints;
+    bool playerPlanted = false;
+    int hintsAccepted = 0;
 
     //hint info
     //These should be in order, i.e. hint 1 should be the first plant down and hint 2 should be the second plant
@@ -27,12 +36,17 @@ public class HintButton : MonoBehaviour
     [SerializeField] int row2;
     [SerializeField] int col2;
 
+    //hint 3 info
+    [SerializeField] GameObject plant3;
+    [SerializeField] int row3;
+    [SerializeField] int col3;
+
     // Start is called before the first frame update
     void Awake()
     {
         hintTracker = GameObject.Find("HintTracker").GetComponent<HintTracker>();
         buttonAudio = GetComponent<AudioSource>();
-        Invoke("ShowHints", .2f);
+        //Invoke("ShowHints", .2f);
         //ShowHintButton();
     }
 
@@ -45,22 +59,44 @@ public class HintButton : MonoBehaviour
 
     public void ShowHintButton()
     {
-        if((hintTracker.getHintsAccepted() >= numHints) || (hintTracker.getLocalFails() <= failTolerance))
+        if((hintTracker.getHintsAccepted() >= numHints) || hintsAccepted >= numHints || playerPlanted)//|| (hintTracker.getLocalFails() <= failTolerance))
         {
-            hintButton.SetActive(false);
+            hintButton.color = new Color(68f / 255f, 68f / 255f, 68f / 255f, 255f / 255f);
+            numHintsDisplay.color = new Color(68f / 255f, 68f / 255f, 68f / 255f, 255f / 255f);
+            displayNumHints.color = new Color(68f / 255f, 68f / 255f, 68f / 255f, 255f / 255f);
+            enableButton.enabled = false;
+            hoverScript.enabled = false;
         }
         else
         {
-            hintButton.SetActive(true);
+            hintButton.color = new Color(255f / 255f, 255f / 255f, 255f / 255f, 255f / 255f);
+            numHintsDisplay.color = new Color(255f / 255f, 255f / 255f, 255f / 255f, 255f / 255f);
+            displayNumHints.color = new Color(255f / 255f, 255f / 255f, 255f / 255f, 255f / 255f);
+            enableButton.enabled = true;
+            hoverScript.enabled = true;
         }
+        displayNumHints.text = "" + (Mathf.Max(0, numHints - hintsAccepted));
     }
 
     public void AcceptHint()
     {
         buttonAudio.Play();
-        hintTracker.AcceptHint();
-        Destroy(hintButton);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        hintsAccepted++;
+        if(hintsAccepted == 1)
+        {
+            ShowHint1();
+        }else if(hintsAccepted == 2)
+        {
+            ShowHint2();
+        }else if(hintsAccepted == 3)
+        {
+            ShowHint3();
+        }
+
+        playerPlanted = false;
+        //hintTracker.AcceptHint(); //used in old hint system
+        //Destroy(hintButton);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     void ShowHints()
@@ -73,7 +109,7 @@ public class HintButton : MonoBehaviour
     void ShowHint1()
     {
         //Debug.LogError("Showing Hint 1");
-        if (hintTracker.getHintsAccepted() >= 1 && plant1 != null)
+        if (hintsAccepted >= 1 && plant1 != null)
         {
             LevelTile tile = lvMan.findTile(row1, col1);
             tile.plantPlant(plant1.GetComponent<PlantCard>().getPlantID());
@@ -84,11 +120,27 @@ public class HintButton : MonoBehaviour
     void ShowHint2()
     {
         //Debug.LogError("Showing Hint 2");
-        if (hintTracker.getHintsAccepted() >= 2 && plant2 != null)
+        if (hintsAccepted >= 2 && plant2 != null)
         {
             LevelTile tile = lvMan.findTile(row2, col2);
             tile.plantPlant(plant2.GetComponent<PlantCard>().getPlantID());
             Destroy(plant2);
         }
+    }
+
+    void ShowHint3()
+    {
+        //Debug.LogError("Showing Hint 2");
+        if (hintsAccepted >= 3 && plant3 != null)
+        {
+            LevelTile tile = lvMan.findTile(row3, col3);
+            tile.plantPlant(plant3.GetComponent<PlantCard>().getPlantID());
+            Destroy(plant3);
+        }
+    }
+
+    public void PlayerPlanted()
+    {
+        playerPlanted = true;
     }
 }
